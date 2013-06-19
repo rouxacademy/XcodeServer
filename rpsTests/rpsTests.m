6e55c4cf0b1f1d869556eaafb7d990b58115524f
Ron Buencamino <ron@Darth-Gopher.local>
2013-06-19 07:18:31 -0700

    Initial Commit

---
+++ rpsTests/rpsTests.m
@@ -0,0 +1,36 @@
+//
+//  rpsTests.m
+//  rpsTests
+//
+//  Created by Ron Buencamino on 6/19/13.
+//  Copyright (c) 2013 Animatronic Gopher Inc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface rpsTests : XCTestCase
+
+@end
+
+@implementation rpsTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
